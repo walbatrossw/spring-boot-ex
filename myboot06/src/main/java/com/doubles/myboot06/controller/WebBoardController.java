4063feeb8d374cab2ead0db6a35ad3c84bff6ca2
DESKTOP-I82LT06\walba <walbatrossw@gmail.com>
2017-11-04 03:53:22 +0900

    7) Spring MVC를 이용한 통합2 - 페이징처리

--- myboot06/src/main/java/com/doubles/myboot06/controller/WebBoardController.java
+++ myboot06/src/main/java/com/doubles/myboot06/controller/WebBoardController.java
@@ -1,7 +1,17 @@
 package com.doubles.myboot06.controller;
 
+import com.doubles.myboot06.domain.WebBoard;
+import com.doubles.myboot06.persistence.WebBoardRepository;
+import com.doubles.myboot06.vo.PageMaker;
+import com.doubles.myboot06.vo.PageVO;
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,10 +20,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Log
 public class WebBoardController {
 
+    @Autowired
+    private WebBoardRepository repo;
+
+
+    // @PageableDefault 를 이용한 페이지 처리
+//    @GetMapping("/list")
+//    public void list(@PageableDefault(direction = Sort.Direction.DESC, sort = "bno", size = 10, page = 0) Pageable page) {
+//        log.info("list() called..." + page);
+//    }
+
+    // PageVo를 생성하는 방식
     @GetMapping("/list")
-    public void list() {
+    public void list(PageVO vo, Model model) {
 
-        log.info("list() called...");
+        Pageable page = vo.makePageable(0, "bno");
 
+        Page<WebBoard> result = repo.findAll(repo.makePredicate(null, null), page);
+        log.info("" + page);
+        log.info("" + result);
+
+        log.info("TOTAL PAGE NUMBER : " + result.getTotalPages());
+
+        //model.addAttribute("result", result);
+        model.addAttribute("result", new PageMaker(result));
     }
 }
--- myboot06/src/test/java/com/doubles/myboot06/WebBoardRepositoryTests.java
+++ myboot06/src/test/java/com/doubles/myboot06/WebBoardRepositoryTests.java
@@ -7,6 +7,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -30,5 +34,27 @@ public class WebBoardRepositoryTests {
             board.setWriter("user0" + (1%10));
             repo.save(board);
         });
+    }
+
+    // 단순 페이징 처리 테스트
+    @Test
+    public void testList1() {
+        Pageable pageable = PageRequest.of(0, 20, Sort.Direction.DESC, "bno");
+        Page<WebBoard> result = repo.findAll(repo.makePredicate(null, null), pageable);
+
+        log.info("PAGE : " + result.getPageable());
+        log.info("--------------------------------");
+        result.getContent().forEach(board -> log.info("" + board));
+    }
+
+    // 검색조건 테스트
+    @Test
+    public void testList2() {
+        Pageable pageable = PageRequest.of(0, 20, Sort.Direction.DESC, "bno");
+        Page<WebBoard> result = repo.findAll(repo.makePredicate("t", "10"), pageable);
+
+        log.info("PAGE: " + result.getPageable());
+        log.info("--------------------------------");
+        result.getContent().forEach(board -> log.info("" + board));
     }
 }
