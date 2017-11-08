ee2d4213a7f98b214d76241167aa24de0740e39b
walbatrossw <walbatrossw@gmail.com>
2017-11-08 17:15:11 +0900

    18) Spring Web Security 3 -  JDBC를 이용한 인증처리

--- myboot08/src/main/java/com/doubles/myboot08/security/SecurityConfig.java
+++ myboot08/src/main/java/com/doubles/myboot08/security/SecurityConfig.java
@@ -7,9 +7,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @Log
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -35,11 +40,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.logout().logoutUrl("/logout").invalidateHttpSession(true);
     }
 
-    // 로그인 정보 설정 하기 inMemory
-   @Autowired
+    // JDBC를 이용한 인증처리
+    @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        log.info("build auth global....");
-        auth.inMemoryAuthentication().withUser("manager").password("1111").roles("MANAGER");
+
+        log.info("build Auth global ...");
+
+        String query1 = "SELECT uid username, upw password, true enabled FROM tbl_members WHERE uid = ?";
+
+        String query2 = "SELECT member uid, role_name role FROM tbl_member_roles WHERE member = ?";
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(query1)
+                .rolePrefix("ROLE_")
+                .authoritiesByUsernameQuery(query2);
     }
+
+
+    // 로그인 정보 설정 하기 inMemory
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        log.info("build auth global....");
+//        auth.inMemoryAuthentication().withUser("manager").password("1111").roles("MANAGER");
+//    }
 
 }
