17fecb2e646d78ac5640906f18528af073e2412a
walbatrossw <walbatrossw@gmail.com>
2017-11-09 00:03:27 +0900

    22) Spring Web Security 6 -  MVC 애너테이션처리 (@Secured)

--- myboot08/src/main/java/com/doubles/myboot08/controller/SampleController.java
+++ myboot08/src/main/java/com/doubles/myboot08/controller/SampleController.java
@@ -1,6 +1,7 @@
 package com.doubles.myboot08.controller;
 
 import lombok.extern.java.Log;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -27,5 +28,19 @@ public class SampleController {
     @GetMapping("/admin")
     public void forAdmin() {
         log.info("admin");
+    }
+
+    // mvc 애너테이션 처리
+    @Secured({"ROLE_ADMIN"})
+    @GetMapping("/adminSecret")
+    public void forAdminSecret() {
+        log.info("admin secret");
+    }
+
+    // mvc 애너테이션 처리
+    @Secured({"ROLE_MANAGER"})
+    @GetMapping("/managerSecret")
+    public void forManagerSecret() {
+        log.info("manager secret");
     }
 }
--- myboot08/src/main/java/com/doubles/myboot08/security/SecurityConfig.java
+++ myboot08/src/main/java/com/doubles/myboot08/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.doubles.myboot08.security;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import javax.sql.DataSource;
 
 @Log
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true) // mvc 애너테이션 처리
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
