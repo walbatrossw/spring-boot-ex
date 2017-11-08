6d42eaf0a42a4485788722dd8c370382f2b64204
walbatrossw <walbatrossw@gmail.com>
2017-11-09 00:09:34 +0900

    23) Spring Web Security 7 - passwordEncoder 사용 (BCrypt)

--- myboot08/src/main/java/com/doubles/myboot08/security/SecurityConfig.java
+++ myboot08/src/main/java/com/doubles/myboot08/security/SecurityConfig.java
@@ -2,11 +2,14 @@ package com.doubles.myboot08.security;
 
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
@@ -62,6 +65,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
         repo.setDataSource(dataSource);
         return repo;
+    }
+
+    // PasswordEncoder 사용하기
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        log.info("build Auth Global...");
+        auth.userDetailsService(usersService).passwordEncoder(passwordEncoder());
     }
 
     // JDBC를 이용한 인증처리
