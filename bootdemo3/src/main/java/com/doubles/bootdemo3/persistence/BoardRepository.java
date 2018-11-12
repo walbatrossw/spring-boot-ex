58a095b85d69d54bf7175bbd6ae73f22a158904c
SangSeob Yoon <walbatrossw@gmail.com>
2018-11-12 15:46:44 +0900

    43nd commit : boot demo3 진행중

--- bootdemo3/src/main/java/com/doubles/bootdemo3/persistence/BoardRepository.java
+++ bootdemo3/src/main/java/com/doubles/bootdemo3/persistence/BoardRepository.java
@@ -1,8 +1,39 @@
 package com.doubles.bootdemo3.persistence;
 
 import com.doubles.bootdemo3.domain.Board;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.Collection;
+import java.util.List;
 
 public interface BoardRepository extends CrudRepository<Board, Long> {
 
+    // findBoardBy
+    public List<Board> findBoardByTitle(String title);
+
+    // findBy
+    public Collection<Board> findByWriter(String writer);
+
+    // like 구문
+    public Collection<Board> findByWriterContaining(String writer);
+
+    // and or 조건
+    public Collection<Board> findByTitleContainingOrContentContaining(String title, String content);
+
+    // 부등호 처리
+    public Collection<Board> findByTitleContainingAndBnoGreaterThan(String keyword, Long num);
+
+    // Order by
+    public Collection<Board> findByBnoGreaterThanOrderByBnoDesc(Long bno);
+
+    // bno > ? order by bno desc limit ?, ?
+    public List<Board> findByBnoGreaterThanOrderByBnoDesc(Long bno, Pageable paging);
+
+    // bno > ?
+    public List<Board> findByBnoGreaterThan(Long bno, Pageable paging);
+
+    //
+    //public Page<Board> findByBnoGreaterThan(Long bno, Pageable paging);
 }
--- bootdemo3/src/test/java/com/doubles/bootdemo3/Bootdemo3ApplicationTests.java
+++ bootdemo3/src/test/java/com/doubles/bootdemo3/Bootdemo3ApplicationTests.java
@@ -6,7 +6,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collection;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,8 +20,9 @@ public class Bootdemo3ApplicationTests {
 	@Autowired
 	private BoardRepository boardRepository;
 
+	// 입력 테스트
 	@Test
-	public void insert() {
+	public void testInsert() {
 		for (int i = 1; i <= 200; i++) {
 			Board board = new Board();
 			board.setTitle("제목..." + i);
@@ -26,5 +32,67 @@ public class Bootdemo3ApplicationTests {
 		}
 	}
 
+	// 게시글 제목으로 조회 테스트
+	@Test
+	public void testByTitle() {
+		boardRepository.findBoardByTitle("제목...177")
+				.forEach(board -> System.out.println(board));
+	}
 
+	// 게시글 작성자로 조회 테스트
+	@Test
+	public void testByWriter() {
+		Collection<Board> results = boardRepository.findByWriter("user00");
+		results.forEach(board -> System.out.println(board));
+	}
+
+	// like 구문 처리 테스트
+	@Test
+	public void testByWriterContaining() {
+		Collection<Board> results = boardRepository.findByWriterContaining("05");
+		results.forEach(board -> System.out.println(board));
+	}
+
+	// and or 조건처리 테스트
+	@Test
+	public void testAndOr() {
+		Collection<Board> results = boardRepository.findByTitleContainingOrContentContaining("02", "내용");
+		results.forEach(board -> System.out.println(board));
+	}
+
+	// 부등호 처리 테스트
+	@Test
+	public void testByTitleAndBno() {
+		Collection<Board> results = boardRepository.findByTitleContainingAndBnoGreaterThan("5", 50L);
+		results.forEach(board -> System.out.println(board));
+	}
+
+	// order by 처리 테스트
+	@Test
+	public void testBnoOrderBy() {
+		Collection<Board> results = boardRepository.findByBnoGreaterThanOrderByBnoDesc(90L);
+		results.forEach(board -> System.out.println(board));
+	}
+
+	// 페이징
+	@Test
+	public void testBnoOrderByPaging() {
+		Pageable paging = new PageRequest(0, 10);
+		Collection<Board> results = boardRepository.findByBnoGreaterThanOrderByBnoDesc(0L, paging);
+		results.forEach(board -> System.out.println(board));
+	}
+
+	// 페이징, 정렬
+	@Test
+	public void testBnoPagingSort() {
+		Pageable paging = new PageRequest(0, 10, Sort.Direction.ASC, "bno");
+		Collection<Board> results = boardRepository.findByBnoGreaterThan(0L, paging);
+		results.forEach(board -> System.out.println(board));
+	}
+
+	// 페이징, 정렬
+	@Test
+	public void testBnoPagingSort2() {
+
+	}
 }
