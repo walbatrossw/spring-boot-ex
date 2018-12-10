b07b34a8d4ec49c3d1c2344e2cb20dbe59a6628f
SangSeob Yoon <walbatrossw@gmail.com>
2018-12-10 15:39:54 +0900

    60th commit : boot demo 7 - 프로젝트 생성 : rest 댓글 처리

---
+++ bootdemo7/src/main/java/com/doubles/bootdemo7/Bootdemo7Application.java
@@ -0,0 +1,12 @@
+package com.doubles.bootdemo7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootdemo7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bootdemo7Application.class, args);
+    }
+}
