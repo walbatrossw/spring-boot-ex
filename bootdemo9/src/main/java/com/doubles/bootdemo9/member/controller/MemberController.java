1eee966dca8c0645fea8e6a1c1c7e3dae0656324
SangSeob Yoon <walbatrossw@gmail.com>
2018-12-21 16:57:03 +0900

    95th commit : boot demo 9 - 회원가입, 비밀번호 암호화 처리

--- bootdemo9/src/main/java/com/doubles/bootdemo9/member/controller/MemberController.java
+++ bootdemo9/src/main/java/com/doubles/bootdemo9/member/controller/MemberController.java
@@ -1,19 +1,52 @@
 package com.doubles.bootdemo9.member.controller;
 
+import com.doubles.bootdemo9.member.domain.Member;
+import com.doubles.bootdemo9.member.persistence.MemberRepository;
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Log
 @Controller
 @RequestMapping("/member")
 public class MemberController {
 
+    private final PasswordEncoder passwordEncoder;
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberController(PasswordEncoder passwordEncoder, MemberRepository memberRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.memberRepository = memberRepository;
+    }
+
     // 회원가입 페이지
     @GetMapping("/register")
     public void register() {
         log.info("register() called ...");
+    }
+
+    // 회원가입 처리
+    @PostMapping("/register")
+    public String register(@ModelAttribute("member")Member member, RedirectAttributes redirectAttributes) {
+
+        log.info("MEMBER : " + member);
+        String encryptPw = passwordEncoder.encode(member.getMemberPw());
+
+        log.info("encoded password : " + encryptPw);
+        member.setMemberPw(encryptPw);
+
+        memberRepository.save(member);
+        redirectAttributes.addFlashAttribute("msg", "register success");
+
+        return "redirect:/member/login";
     }
 
     // 로그인 페이지
--- bootdemo9/src/main/java/com/doubles/bootdemo9/security/SecurityConfig.java
+++ bootdemo9/src/main/java/com/doubles/bootdemo9/security/SecurityConfig.java
@@ -2,9 +2,14 @@ package com.doubles.bootdemo9.security;
 
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import javax.sql.DataSource;
 
@@ -43,8 +48,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/member/login")  // 로그인 성공 이후 페이지
                 .invalidateHttpSession(true);   // 세션 무효화
 
-        http.userDetailsService(usersService);
+        http.rememberMe()
+                .key("doubles")
+                .userDetailsService(usersService)
+                .tokenRepository(getJDBCRepository())
+                .tokenValiditySeconds(60*60*24);
+    }
 
+    private PersistentTokenRepository getJDBCRepository() {
+        JdbcTokenRepositoryImpl repository = new JdbcTokenRepositoryImpl();
+        repository.setDataSource(dataSource);
+        return repository;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
